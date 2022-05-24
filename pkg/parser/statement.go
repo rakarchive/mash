2c930ea9ae43f7650fbfd08cae1226984cbbfd9b
Rak Laptudirm <raklaptudirm@gmail.com>
2022-05-24 18:12:00 +0530

    chore: update copyright years

--- pkg/parser/statement.go
+++ pkg/parser/statement.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 Rak Laptudirm <raklaptudirm@gmail.com>
+// Copyright © 2022 Rak Laptudirm <raklaptudirm@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
